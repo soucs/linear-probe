fd27b71da11d8a558e0f13d2d35c0bf44946e372
soucs <73250961+soucs@users.noreply.github.com>
2026-04-05 10:46:28 +0530

    Removed prompt template for continuous generation

--- inference.py
+++ inference.py
@@ -5,17 +5,11 @@ adapter_path = "adapters"
 model_path = "mlx-community/Qwen3.5-0.8B-4bit"
 model_lora, tokenizer = load(model_path, adapter_path=adapter_path)
 
-prompt = "The truth is rarely pure and never simple. ->: "
-messages = [{"role": "user", "content": prompt}]
-prompt = tokenizer.apply_chat_template(
-    messages, 
-    tokenize=False, 
-    add_generation_prompt=True
-)
+prompt = "I'm quite illiterate, but I read a lot. ->: "
 
 response = generate(
     model_lora, 
     tokenizer, 
     prompt=prompt, 
     verbose=True
-    )
+)
